7eadf42f281255d761b5020e13dd1411ba983ea7
usmanismail <0xfffffff@gmail.com>
2014-01-01 22:06:18 -0500

    Adding support for client modes

--- com/techtraits/gpns/gpnsconfig/parser.go
+++ com/techtraits/gpns/gpnsconfig/parser.go
@@ -6,12 +6,30 @@ import (
 	"log"
 )
 
-func ParseConfig() {
+type APPLICATION_MODE int
+
+const (
+	SERVER_MODE   APPLICATION_MODE = iota
+	REGISTER_MODE APPLICATION_MODE = iota
+	SEND_MODE     APPLICATION_MODE = iota
+)
+
+func ParseConfig() APPLICATION_MODE {
 	var aws_config_file string
 	var base_config_file string
+	var register bool
+	var send bool
+	var input_file string
+	var output_file string
 
 	flag.StringVar(&base_config_file, "baseConfig", "./config/base.conf", "The path to the base configuration file")
 	flag.StringVar(&aws_config_file, "awsConfig", "./config/aws.conf", "The path to the aws configuration file")
+
+	flag.BoolVar(&register, "register", false, "Set flag to run in client mode and register a set of devices. If true inputFile and outputFile must be set.")
+	flag.BoolVar(&send, "send", false, "Set flag to run in client mode and send push notifications to a set of arns. If true inputFile and outputFile must be set.")
+
+	flag.StringVar(&input_file, "inputFile", "", "The path to the Device IDs or Arns file")
+	flag.StringVar(&output_file, "outputFile", "", "The path to the Device IDs or Arns file")
 	flag.Parse()
 
 	log.Printf("Using base configuration file: %s", base_config_file)
@@ -24,6 +42,17 @@ func ParseConfig() {
 
 	parseBaseConfig(baseConfig)
 	parseAwsConfig(awsConfig)
+
+	if register {
+		log.Printf("Running in client mode, registering devices listed in %s, and printing arns in %s", input_file, output_file)
+		return REGISTER_MODE
+	} else if send {
+		log.Printf("Running in client mode, sending pusn notes to ARNs listed in %s, and printing results in %s", input_file, output_file)
+		return SEND_MODE
+	} else {
+		log.Printf("Running in server mode")
+		return SERVER_MODE
+	}
 
 }
 
--- main.go
+++ main.go
@@ -7,21 +7,13 @@ import (
 
 func main() {
 
-	gpnsconfig.ParseConfig()
-	rest.SetupRestServices()
+	appMode := gpnsconfig.ParseConfig()
+	if appMode == gpnsconfig.SERVER_MODE {
+		rest.SetupRestServices()
+	} else if appMode == gpnsconfig.REGISTER_MODE {
 
-	/*
+	} else if appMode == gpnsconfig.SEND_MODE {
 
-
-		publisher := sns.InitilizePublisher(awsConfig)
-		arn, err := registrar.RegisterDevice("Test", "APA91bFaRKjCZfNcAhPTw6wSFGUxRi3108G_Swnz0fZ-Xr2pK9bwMGBjntXEJ72nrIyodMNx49cO3KESBpM3Jmd0zMpHToo1Cb_zR-_Lzqt5B-GRnzx3UuRHL6D6G9xaQwLLn05ugPjMm5Z8fLSTWocwT9ozCANcrqdM4tG-ljf7N3H7iSeymvo", "EN_US")
-		if err != nil {
-			log.Fatalf("Unable to register device %s", err.Error())
-		}
-		err = publisher.PublishNotification("Test", arn, "Title From GO", "Text From Go")
-		if err != nil {
-			log.Fatalf("%s", err.Error())
-		}
-	*/
+	}
 
 }
