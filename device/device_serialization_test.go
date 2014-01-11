fd2b3f73e9698fcc42d93ef4eb385520f0f39d6b
usmanismail <0xfffffff@gmail.com>
2014-01-11 10:23:11 -0500

    Adding unit tests to travis CI

---
+++ device/device_serialization_test.go
@@ -0,0 +1,25 @@
+package device
+
+import (
+	"testing"
+)
+
+func TestLocaleValidation(t *testing.T) {
+
+	if ValidateLocale("en_US") != nil {
+		t.Error("en_US is a valid locale but code is saying it isn't")
+	}
+
+	if ValidateLocale("en_US1") == nil {
+		t.Error("en_US1 is not a valid locale but code is saying it is")
+	}
+
+	if ValidateLocale("enUS") == nil {
+		t.Error("enUS is not a valid locale but code is saying it is")
+	}
+
+	if ValidateLocale("en_USSS") == nil {
+		t.Error("en_USSS is not a valid locale but code is saying it is")
+	}
+
+}
