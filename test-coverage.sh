b8cee423760d0a5fabdd87e57f518ac408f38f1f
usmanismail <0xfffffff@gmail.com>
2014-01-14 19:02:17 -0500

    Testing Coveralls integration

---
+++ test-coverage.sh
@@ -0,0 +1,10 @@
+
+echo "mode: set" > acc.out
+for Dir in $(find ./* -maxdepth 10 -type d ); 
+do
+   go test -coverprofile=profile.out $Dir
+   cat profile.out | grep -v "mode: set" >> acc.out
+done
+goveralls -coverprofile=acc.out $COVERALLS-KEY
+rm -rf ./profile.out
+rm -rf ./acc.out
