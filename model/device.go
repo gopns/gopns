c075b1d43c7fec5e01f246145fb19ae887ccb67a
Bilal Sheikh <bilalsheikh298@gmail.com>
2014-01-17 00:26:52 -0500

    update device model

--- model/device.go
+++ model/device.go
@@ -5,46 +5,81 @@ import (
 	"regexp"
 )
 
-var validLocale = regexp.MustCompile(`^[A-Za-z]{2,3}_[A-Za-z]{2,3}$`)
+type DeviceType string
 
-//TODO Verify IOS and ADM as keys
-var validPlatform = regexp.MustCompile(`^(GCM|ADM|IOS)$`)
+const (
+	IOS     DeviceType = "IOS"
+	ANDROID DeviceType = "ANDROID"
+	KINDLE  DeviceType = "KINDLE"
+)
 
-type DeviceRegistration struct {
-	Alias       string
-	Id          string
-	Locale      string
-	PlatformApp string
-	Tags        []string
-}
-
+//TODO: constants for locale and timezone
 type Device struct {
-	Alias    string
-	Locale   string
-	Arns     []string
-	Platform string
-	Tags     []string
+	userId     string
+	appId      string
+	deviceType DeviceType
+	arn        string
+	token      string
+	locale     string
+	timezone   string
+	enabled    bool
 }
 
-type DeviceList struct {
-	Devices []Device
-	Cursor  string `json:",omitempty"`
+func (device Device) UserId() string {
+	return device.userId
 }
 
-func (this DeviceRegistration) ValidateLocale() error {
-	//TODO More validation to actually check codes
-	if !validLocale.MatchString(this.Locale) {
-		return errors.New("Invalid locale")
+func (device Device) AppId() string {
+	return device.appId
+}
+
+func (device Device) Locale() string {
+	return device.locale
+}
+
+func (device Device) DeviceType() DeviceType {
+	return device.deviceType
+}
+
+func (device Device) Arn() string {
+	return device.arn
+}
+
+func (device Device) Token() string {
+	return device.token
+}
+
+func (device Device) Enabled() bool {
+	return device.enabled
+}
+
+func (device Device) Timezone() string {
+	return device.timezone
+}
+
+func (device *Device) SetUserId(id string) {
+	device.userId = id
+}
+
+func (device *Device) SetAppId(id string) {
+	device.appId = id
+}
+
+var localRegex = regexp.MustCompile(`^[A-Za-z]{2,3}_[A-Za-z]{2,3}$`)
+
+func ValidateLocale(locale string) error {
+	if !localRegex.MatchString(locale) {
+		return errors.New("Invalid locale, should be of the form US_en.")
 	}
 
 	return nil
-
 }
 
-func ValidateLocale(locale string) error {
-	//TODO More validation to actually check codes
-	if !validLocale.MatchString(locale) {
-		return errors.New("Invalid locale")
+var deviceTypeRegex = regexp.MustCompile("^(" + string(ANDROID) + "|" + string(IOS) + "|" + string(KINDLE) + ")$")
+
+func ValidateDeviceType(dt DeviceType) error {
+	if !deviceTypeRegex.MatchString(string(dt)) {
+		return errors.New("Invalid device type, valid values: " + string(ANDROID) + "," + string(IOS) + " or " + string(KINDLE))
 	}
 
 	return nil
